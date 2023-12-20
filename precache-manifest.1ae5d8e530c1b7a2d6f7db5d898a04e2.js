bc3a293b322771157ed49881fb5426cb1b2bfc1b
Sanskriti <sanskritimutreja@gamil.com>
2023-12-20 15:21:10 +0530

    Updates

---
+++ precache-manifest.1ae5d8e530c1b7a2d6f7db5d898a04e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f309d6dec483b1b48341664014c0d359",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "5c9caab680f5c9ce05b4",
+    "url": "/RoboFriends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "eb5a2d567e539b3bf1b1",
+    "url": "/RoboFriends/static/css/main.5b222f02.chunk.css"
+  },
+  {
+    "revision": "5c9caab680f5c9ce05b4",
+    "url": "/RoboFriends/static/js/2.99ae3eca.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/RoboFriends/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "eb5a2d567e539b3bf1b1",
+    "url": "/RoboFriends/static/js/main.81a34a4d.chunk.js"
+  },
+  {
+    "revision": "743f2bf4b0c07794547b",
+    "url": "/RoboFriends/static/js/runtime~main.d425f0ad.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
